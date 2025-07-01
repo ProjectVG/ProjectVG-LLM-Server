3fa2d10e0846e2f97ed7ad23bfc2f10cc920170b
WooSH <imdlsrks.mc@gmail.com>
2025-07-01 19:36:01 +0900

    feat: 기본 채팅 기능 구현
    - 기본적인 채팅 기능 구현
    - system message와 prompt message, model 값만 입력력

--- main.py
+++ main.py
@@ -13,5 +13,23 @@ def check_api_key():
         print("OPENAI_API_KEY를 불러오지 못함. .env 파일 또는 환경변수를 확인하세요.")
 
 
+
+def chat_with_openai(messages, model="gpt-4o-mini", temperature=0.7):
+    response = openai.responses.create(
+        model=model,
+        input=messages,
+        temperature=temperature
+    )
+    return response.output_text
+
+
 if __name__ == "__main__":
     check_api_key()
+
+    messages = [
+        {"role": "system", "content": "너는 친구 AI야. 반말로 짧게 말해줘."},
+        {"role": "user", "content": "오늘 무슨일 있니?"}
+    ]
+
+    reply = chat_with_openai(messages)
+    print("AI:", reply)
